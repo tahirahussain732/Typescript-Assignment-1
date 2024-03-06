f6eb40d29792174ef17130b35f37013f030200bc
Tahirahussain <66288117+tahirahussain732@users.noreply.github.com>
2024-03-07 02:13:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+"use strict";
+//Test =1
+let car = ('Toyota');
+console.log("is car=='Toyota'?i predict true");
+console.log(car == 'Toyota');
+//Test =2
+let fruit = ('apple');
+console.log("is fruit!=='mango'?i predict false");
+console.log(fruit == 'mango');
+//test =3
+let riceprice = 150;
+console.log("is riceprice < 200 ? i predict true");
+console.log(riceprice<200);
+
+//test =4
+let breadprice = 150;
+console.log("is breadprice > 100 ? i predict true");
+console.log(breadprice>100);
+
+// test 5
+let laptopprice = 80000;
+console.log("is laptopprice <= 80000?i predict true")
+console.log(laptopprice<=80000);
+
+// test 6
+let mobileprice = 150000;
+console.log("is mobileprice >= 80000?i predict true")
+console.log(mobileprice>=1550000);
+
+//test 7
+let tea='Tapal';
+console.log("is tea == 'lipton'?i predict false");
+console.log(tea=='lipton');
+
+//test 8
+let computer='Intel';
+console.log("is computer === 'Hp'?i predict false");
+console.log(computer === 'Hp');
+
+//test 9
+let agefahad=20;
+console.log("is agefahad < 20?i predict false");
+console.log(agefahad<20);
+
+//test 10
+let flower='Rose';
+console.log("is flower =='Tulip?i predict false")
+console.log(flower=='Tulip');
---
+++ main.ts
@@ -0,0 +1,14 @@
+//Test =1
+let car: string =('Toyota');
+console.log("is car=='Toyota'?i predict true")
+console.log(car=='Toyota')
+
+//Test =2
+let fruit : string =('apple');
+console.log("is fruit!=='mango'?i predict false")
+console.log(fruit=='mango')
+
+//test =3
+let riceprice : number = 150;
+console.log("is riceprice < 150 ? i predict true")
+console.log('riceprice<150');
